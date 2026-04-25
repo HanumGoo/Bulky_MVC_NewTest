effbfd6f54ba7c8bd150a7f87b0d1ee945e75973
sheehan <sheehanandya001@gmail.com>
2026-04-25 18:43:48 +0700

    adding entity framework core and framework core for SSMS

---
+++ BulkyWeb1/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
